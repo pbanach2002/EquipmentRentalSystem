9675cc03dd87eda63dfa822f99bdab4acb68f1fd
pbanach2002 <s29442@pjwstk.edu.pl>
2026-03-30 16:51:24 +0200

    add Rental model

---
+++ Models/Rental.cs
@@ -0,0 +1,12 @@
+﻿namespace EquipmentRentalSystem.Models;
+
+public class Rental
+{
+    public Equipment Equipment { get; set; }
+    public User User { get; set; }
+    public DateTime RentDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    
+    public bool isLate => ReturnDate > DueDate;
+}
