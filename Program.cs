ddf826793c3ec67382347e39a64c8bc115a77aec
pbanach2002 <s29442@pjwstk.edu.pl>
2026-03-30 17:20:59 +0200

    add demo scenario in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EquipmentRentalSystem.Models;
+using EquipmentRentalSystem.Services;
 
-Console.WriteLine("Hello, World!");
+var service = new RentalService();
+
+var student = new Student
+{
+    FirstName = "Patryk",
+    LastName = "Banach"
+};
+
+var laptop = new Laptop
+{
+    Name = "Dell Precision 5560",
+    Ram = 32,
+    Processor = "i7"
+};
+
+Console.WriteLine("=== RENT ===");
+service.RentEquipment(student, laptop);
+
+Console.WriteLine("=== BAD TRY ===");
+try
+{
+    service.RentEquipment(student, laptop);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.WriteLine("=== RETURN ===");
+var rental = service.GetActiveRentals(student).FirstOrDefault();
+
+if (rental == null)
+{
+    Console.WriteLine("No active rental");
+    return;
+}
+service.ReturnEquipment(rental);
