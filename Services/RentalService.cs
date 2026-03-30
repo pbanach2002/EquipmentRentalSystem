274c81db5b820ef80283ab290e7bb915b18c86f1
pbanach2002 <s29442@pjwstk.edu.pl>
2026-03-30 17:00:26 +0200

    implement basic renting logic

--- Services/RentalService.cs
+++ Services/RentalService.cs
@@ -4,4 +4,20 @@ namespace EquipmentRentalSystem.Services;
 public class RentalService
 {
     private List<Rental> rentals = new();
+
+    public void RentEquipment(User user, Equipment equipment)
+    {
+        if(!equipment.IsAvailable)
+            throw new ArgumentException("Equipment is not available");
+
+        var rental = new Rental
+        {
+            User = user,
+            Equipment = equipment,
+            RentDate = DateTime.Now,
+            ReturnDate = DateTime.Now.AddDays(7)
+        };
+        rentals.Add(rental);
+        equipment.IsAvailable = false;
+    }
 }
